ffeb3ebdee3f03b9fdb334b6b6341d6c111498d1
Kushagra Upadhyay <45212589+Kushagraupadhyay@users.noreply.github.com>
2019-01-15 22:56:20 +0530

    Update noofdigitsininput.cpp

--- Beginner/Set1/noofdigitsininput.cpp
+++ Beginner/Set1/noofdigitsininput.cpp
@@ -1,45 +1,12 @@
 #include<iostream>
-#include<cstdio>
-using namespace std;
-bool arraypass(char x[])
-{
-      int i = 0,j,flag=0;
-      while (x[i] != '\0')
-      {
-          ++i;
-      }
-  for(j=0;j<i;j++)
-  {
-    if(isdigit(x[j])==0)
-    {
-      flag=1;
-      break;
-    }
-  }
-  if(flag==0)
-  {
-    return true;
-  }
-  else
-  return false;
-}
-
 int main()
 {
-  int a,nod=0;
-char x[10],y;
-scanf("%s",&x);
-if(arraypass(x))
-{
-  a=atoi(x);
-  while(a>0)
+  int n,nod=0;
+  scanf("%d",&n);
+  while(n!=0)
   {
-    a=a/10;
     nod+=1;
+    n=n/10;
   }
   printf("%d",nod);
-}
-else
-printf("Invalid Input");
-return 0;
 }
