bab8e758c980f88c94648cea550aa175382e99f6
Kushagra Upadhyay <45212589+Kushagraupadhyay@users.noreply.github.com>
2019-01-15 17:26:56 +0530

    Create leapyear.cpp

---
+++ Beginner/Set1/leapyear.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+bool arraypass(char x[])
+{
+      int i = 0,j,flag=0;
+      while (x[i] != '\0')
+      {
+          ++i;
+      }
+  for(j=0;j<i;j++)
+  {
+    if(isdigit(x[j])==0)
+    {
+      flag=1;
+      break;
+    }
+  }
+
+  if(flag==0)
+  {
+    return true;
+  }
+  else
+  return false;
+}
+
+int main()
+{
+  int a;
+char x[10],y;
+scanf("%s",&x);
+if(arraypass(x))
+{
+  a=atoi(x);
+  if(a%400==0)
+  printf("Leap Year");
+  else if (a%100==0)
+  printf("Not a Leap Year");
+  else if(a%4==0)
+  printf("Leap Year");
+  else
+  printf("Not a Leap Year");
+}
+else
+printf("Invalid Input");
+return 0;
+}
