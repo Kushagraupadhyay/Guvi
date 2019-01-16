1f54aa223ceb2828f3d537caa5bbeb49df7df859
Kushagra Upadhyay <45212589+Kushagraupadhyay@users.noreply.github.com>
2019-01-16 19:45:28 +0530

    Create repetedelementsdisplay.cpp

---
+++ Pro/Set1/repetedelementsdisplay.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+bool check(int b[],int n,int m)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    if(b[i]==m)
+    return false;
+    else
+    return true;
+  }
+}
+int main()
+{
+  int a[50],b[50],i,j,count=0,n;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(j=0;j<n;j++)
+  {
+    for(i=j+1;i<n;i++)
+    {
+      if(a[j]==a[i])
+      {
+        if(check(b,n,a[i]))
+        {
+          b[count]=a[j];
+          count++;
+        }
+        else
+        continue;
+      }
+    }
+  }
+  for(i=0;i<count;i++)
+  printf("%d ",b[i]);
+  return 0;
+}
