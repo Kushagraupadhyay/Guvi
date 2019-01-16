f84f5fd536b9911370ff26d57168c7fb49735328
Kushagra Upadhyay <45212589+Kushagraupadhyay@users.noreply.github.com>
2019-01-16 20:02:05 +0530

    Update repetedelementsdisplay.cpp

--- Pro/Set1/repetedelementsdisplay.cpp
+++ Pro/Set1/repetedelementsdisplay.cpp
@@ -14,6 +14,8 @@ int main()
 {
   int a[50],b[50],i,j,count=0,n;
   scanf("%d",&n);
+  if(n>0 && n<100000)
+  {
   for(i=0;i<n;i++)
   {
     scanf("%d",&a[i]);
@@ -34,7 +36,19 @@ int main()
       }
     }
   }
+  if(count==0)
+  {
+    printf("Unique");
+  }
+  else
+  {
   for(i=0;i<count;i++)
   printf("%d ",b[i]);
+ }
+}
+else
+{
+  printf("Invalid Input");
+}
   return 0;
 }
