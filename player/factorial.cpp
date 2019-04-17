e5353500f3ee7bc2e2c91387cbdf18cbee074f61
Kushagra Upadhyay <45212589+Kushagraupadhyay@users.noreply.github.com>
2019-04-17 22:27:27 +0530

    Create factorial.cpp

---
+++ player/factorial.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a[50],n,i,fact;
+  scanf("%d",&n);
+  if(n<0)
+  {
+    printf("Enter a positive number");
+  }
+  else
+  {
+    if(n==0)
+    {
+      printf("0");
+    }
+    else
+    {
+      fact=1;
+      for(i=n;i>=1;i--)
+      {
+        fact*=i;
+      }
+      printf("%d",fact);
+    }
+
+  }
+
+  return 0;
+}
